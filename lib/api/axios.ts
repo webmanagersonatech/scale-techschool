f171198b1daafb1fae59c24e28d420ee6e2b0a19
vinoth <webdeveloper2@sonatech.ac.in>
2026-04-22 12:53:03 +0530

    push

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-// const API_BASE_URL =
-//   process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
   const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://160.187.54.80:8000";
+  process.env.NEXT_PUBLIC_API_URL || "http://161.248.37.193:8000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
